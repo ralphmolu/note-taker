8225017ba34d94ed3c349accb34b3f9d328cffb9
Ralph Molu <rmolu11@gmail.com>
2024-04-21 19:25:44 -0400

    tryni to ensure heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// dependencies
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT || 3006;
+
+app.get('/', (req, res)=>{
+console.log('test API endpoint')
+res.end('Your api is working, you are at the home page');
+});
 
 app.listen(PORT, (req, res) =>{
     console.log(`Server running on port: ${PORT}`);
